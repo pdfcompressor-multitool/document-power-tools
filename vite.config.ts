dce7f461eb643d6cea83b843368bef70cc9ae402
pdfcompressor-multitool <vedantkumkar34@gmail.com>
2026-01-09 01:09:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
 export default defineConfig({
+  // IMPORTANT: repo name here
   base: "/document-power-tools/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
     outDir: "dist",
   },
